4937f8b8bb461f561fa5d75d038c6dc709b22ff1
dhslrl321 <dhslrl321@gmail.com>
2022-05-23 13:47:09 +0900

    commit

---
+++ indexer/src/main/java/com/example/indexer/CrewSearchController.java
@@ -0,0 +1,10 @@
+package com.example.indexer;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class CrewSearchController {
+
+}
